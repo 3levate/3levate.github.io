7390748506bddb63b201e3f73afd491c17941469
Jason Firkus <jacecraft29@gmail.com>
2025-01-22 11:40:51 -0800

    update

---
+++ scripts/handleMediaQueries.js
@@ -0,0 +1,66 @@
+const maxWidthQuery = window.matchMedia("(min-width: 1281px) and (max-width: 1600px)");
+const smallerWidthsQuery = window.matchMedia("(max-width: 1280px)");
+const navContactClone = document.getElementById("nav-additional-info");
+const newsletterClone = document.getElementById("newsletter");
+
+function checkForMaxWidthMedia(mediaQuery) {
+  const container = document.querySelector("#container");
+  const steaksGallery = document.querySelector(".steaks-gallery");
+  const steaksBanner = document.querySelector("#steaks-banner");
+  let steaks = document.querySelector(".steaks");
+
+  //if it is max width screen size then ensure steaks for flexbox with gallery and banner
+  if (mediaQuery.matches) {
+    if (!steaks) {
+      steaks = document.createElement("div");
+      steaks.classList.add("steaks");
+      container.appendChild(steaks);
+      console.log("created steaks");
+    }
+
+    if (!steaks.contains(steaksGallery) && !steaks.contains(steaksBanner)) {
+      steaks.appendChild(steaksGallery);
+      steaks.appendChild(steaksBanner);
+      console.log("added steaks gallery and steaks-banner to steaks");
+    }
+  } else if (steaks) {
+    container.insertBefore(steaksGallery, steaks);
+    container.insertBefore(steaksBanner, steaks);
+    steaks.remove();
+    console.log("removed steaks");
+  }
+}
+
+function checkForSmallerMedia(smallerMediaQuery) {
+  const header = document.querySelector("header");
+  let navContact = document.getElementById("nav-additional-info");
+
+  if (smallerMediaQuery.matches && navContact) {
+    navContact.remove();
+    console.log("removed nav-additional-info");
+  } else if (!navContact) {
+    header.appendChild(navContactClone);
+    console.log("added nav-additional-info");
+  }
+}
+
+function checkForSmallerMedia_newsLetter(smallerMediaQuery) {
+  const container = document.querySelector("#container");
+  let newsletter = document.getElementById("newsletter");
+
+  if (smallerMediaQuery.matches && newsletter) {
+    newsletter.remove();
+    console.log("removed newsletter");
+  } else if (!newsletter) {
+    container.appendChild(newsletterClone);
+    console.log("added newsletter");
+  }
+}
+
+maxWidthQuery.addListener(checkForMaxWidthMedia);
+smallerWidthsQuery.addListener(checkForSmallerMedia);
+smallerWidthsQuery.addListener(checkForSmallerMedia_newsLetter);
+
+checkForMaxWidthMedia(maxWidthQuery);
+checkForSmallerMedia(smallerWidthsQuery);
+checkForSmallerMedia_newsLetter(smallerWidthsQuery);
